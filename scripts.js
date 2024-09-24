c9b6f9e51ed75c662f4149a685bd70ac56ce0902
henrynascimentosilva <henrynascimentosilva@gmail.com>
2024-09-24 20:05:47 +0000

    removendo paginação

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     breakpoints: {
         0: {
             slidesPerView: 1
